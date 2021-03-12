f81cfaa28516235a44a6aff8b153e9d8c5dc4335
claire <clairebontempo@gmail.com>
2021-03-12 07:06:25 -0800

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { removeQuote } from '../actions/quotes'
+import { upvoteQuote } from '../actions/quotes'
+import { downvoteQuote } from '../actions/quotes'
 
 class Quotes extends Component {
 
@@ -15,6 +18,9 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
+              {this.props.quotes.map(quote => {
+              return < QuoteCard quoteId={quote.id} quote={quote} removeQuote={this.props.removeQuote} upvoteQuote={this.props.upvoteQuote} downvoteQuote={this.props.downvoteQuote} />
+              })}
               {/*
                 TODO:
 
@@ -28,5 +34,20 @@ class Quotes extends Component {
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    quotes: state.quotes
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    upvoteQuote: (quoteId) => {dispatch(upvoteQuote(quoteId))},
+    downvoteQuote: (quoteId) => {dispatch(downvoteQuote(quoteId))},
+    removeQuote: (quoteId) => {dispatch(removeQuote(quoteId))}
+   
+  }
+}
+
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(mapStateToProps, mapDispatchToProps)(Quotes);
